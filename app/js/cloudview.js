6a3535019407a40af30178ec754bf3bafb469bfe
Ben Sorohan <ben.sorohan@gmail.com>
2014-01-05 17:54:55 +1000

    Updated so that it resolves Parameters and recursively loads sub-stacks.
    Lots of nested promises are quite ugly, but not sure how to handle them better.

--- app/js/cloudview.js
+++ app/js/cloudview.js
@@ -1,26 +1,30 @@
 'use strict';
 
+/* Constants */
+var AWS_CloudFormation_Stack = 'AWS::CloudFormation::Stack';
+
 /* Controllers */
 
 var cloudviewApp = angular.module('cloudviewApp', [
 ]);
 
-cloudviewApp.controller('CloudviewCtrl', function($scope, $http)
+cloudviewApp.controller('CloudviewCtrl', function($scope, $http, $q)
 {
     $scope.loadStack = function()
     {
         if ($scope.templateUrl) {
-            $http.get($scope.templateUrl).success(function(data) {
-                $scope.stack = loadStack(data);
-        //        $scope.stack = CloudStack.query();
+            loadStackTemplateFromUrl($scope.templateUrl).then(function(result) {
+                $scope.stackTemplate = result;
+                $scope.updateStackFromTemplate();
             });
         }
     };
 
-    $scope.updateStackParams = function()
+    $scope.updateStackFromTemplate = function()
     {
-        if ($scope.stack && $scope.stack.Parameters) {
-        }
+        loadStackFromTemplate($scope.stackTemplate).then(function(result) {
+            $scope.stack = result;
+        });
     };
 
     $scope.jsonStringify = JSON.stringify;
@@ -29,11 +33,43 @@ cloudviewApp.controller('CloudviewCtrl', function($scope, $http)
         return !obj || angular.equals(obj, {});
     };
 
-    var loadStack = function(stackTemplate)
+    var loadStackTemplateFromUrl = function(templateUrl)
     {
+        var deferred = $q.defer();
+
+        if (templateUrl) {
+            $http.get(templateUrl).success(function(data) {
+                deferred.resolve(data);
+            });
+        }
+        else {
+            deferred.resolve(false);
+        }
+
+        return deferred.promise;
+    };
+
+    var loadStackFromTemplate = function(stackTemplate)
+    {
+        var deferred = $q.defer();
+
         var stack = {};
+
         // Load params.
-        stack.Parameters = {};
+        stack.Parameters = loadStackParameters(stackTemplate, stack);
+
+        // Load resources.
+        loadStackResources(stackTemplate, stack).then(function(result) {
+            stack.Resources = result;
+            deferred.resolve(stack);
+        });
+
+        return deferred.promise;
+    };
+
+    var loadStackParameters = function(stackTemplate, stack)
+    {
+        var parameters = {};
         if (stackTemplate.Parameters) {
             angular.forEach(stackTemplate.Parameters, function(param, paramName)
             {
@@ -41,10 +77,122 @@ cloudviewApp.controller('CloudviewCtrl', function($scope, $http)
                     // Set param default value.
                     param.Value = param.Default;
                 }
-                stack.Parameters[paramName] = param;
+                parameters[paramName] = param;
             }, stack);
         }
-        return stackTemplate;
+
+        return parameters;
+    };
+
+    var loadStackResources = function(stackTemplate, stack)
+    {
+        var deferred = $q.defer();
+
+        var resources = {};
+        var delayed = {};
+        var attempts = 0;
+        var maxAttempts = 100;
+
+        var isResouceRefsMet = function(resource, stack)
+        {
+            return true;
+        };
+
+        var loadResource = function(resource, resourceName)
+        {
+            var deferred = $q.defer();
+
+            if (resource.Type === AWS_CloudFormation_Stack) {
+                // Load from template URL is asynchronous & recursive.
+                loadStackTemplateFromUrl( resolveResourceProperty(resource, 'TemplateURL', stack) ).then(function(result) {
+                    resource.StackTemplate = result;
+                    if (result) {
+                        // Template loaded, now load the sub-stack as a resource.
+                        loadStackFromTemplate(resource.StackTemplate).then(function(result) {
+                            resource.Stack = result;
+                            deferred.resolve(resource);
+                        })
+                    }
+                    else {
+                        // Template failed to load.
+                        resource.Stack = null;
+                        deferred.resolve(resource);
+                    }
+                });
+            }
+            else {
+                deferred.resolve(resource);
+            }
+
+            return deferred.promise;
+        };
+
+        if (stackTemplate.Resources) {
+            // Process resources until all done, or give up.
+            var resourcesToLoad = angular.copy(stackTemplate.Resources);
+            while (!$scope.isEmpty(resourcesToLoad) && attempts < maxAttempts)
+            {
+                angular.forEach(resourcesToLoad, function(resource, resourceName)
+                {
+                    if (isResouceRefsMet(resource, stack)) {
+                        // Resources met, load then check if we can resolve our promise to load all.
+                        loadResource(resource, resourceName).then(function(result) {
+                            resources[resourceName] = result;
+                            if (Object.keys(resources).length === Object.keys(stackTemplate.Resources).length) {
+                                // That's all promises met (resources all loaded).
+                                deferred.resolve(resources);
+                            }
+                        });
+
+                        // Delete from resource queue (even if it's still loading asynchonously,
+                        // remove it from the list of resources that still need loading.
+                        delete resourcesToLoad[resourceName];
+                    }
+                    // else, resource stays in resourceToLoad until refs are met.
+                });
+                attempts++;
+            }
+        }
+
+        if (!$scope.isEmpty(delayed)) {
+            throw "Resource refs not met."
+        }
+
+        return deferred.promise;
+    };
+
+    var resolveResourceProperty = function(resource, propertyName, stack)
+    {
+
+        if (!resource.Properties || ('undefined' === typeof resource.Properties[propertyName])) {
+            return null;
+        }
+
+        var property = resource.Properties[propertyName];
+
+        if (angular.isString(property)) {
+            return property;
+        }
+        else if (angular.isObject(property)) {
+            if (typeof property.Ref !== 'undefined') {
+                // Property is a { Ref : something }.
+                return resolveStackRef(property.Ref, stack);
+            }
+            else {
+                return property;
+            }
+        }
+    };
+
+    var resolveStackRef = function(ref, stack)
+    {
+        // Check Parameters.
+        if (stack.Parameters && stack.Parameters[ref]) {
+            return stack.Parameters[ref].Value;
+        }
+        else {
+            return null;
+        }
     };
 });
 
